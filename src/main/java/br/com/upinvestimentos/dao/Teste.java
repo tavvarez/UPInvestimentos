767c624dab3c97cea5c906a82b5011f7b0102848
gslblack <gsales.laborao@hotmail.com>
2023-11-19 16:56:40 -0300

    Filtrar usuario por ID

--- src/main/java/br/com/upinvestimentos/dao/Teste.java
+++ src/main/java/br/com/upinvestimentos/dao/Teste.java
@@ -1,52 +1,30 @@
 package br.com.upinvestimentos.dao;
 
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-
 import br.com.upinvestimentos.model.UsuarioModel;
-
 
 public class Teste {
 
-	public static void main(String[] args) throws Exception {
-
-		System.out.println("********************************");
-		System.out.println("CONEXÃO COM O BANCO DE DADOS");
-
-		
-//        GastosDAO daoConsulta = new GastosDAO(0, null, 0);
-//        List<GastosDAO> listaControleGastosGeral = daoConsulta.getAll();
-//
-//        for (GastosDAO item : listaControleGastosGeral) {
-//            System.out.println(item.getCdTransacao() + " " + item.getIdUser() + " " + item.getValorSaldo());
-//        }
-
+    public static void main(String[] args) {
         System.out.println("********************************");
         System.out.println("CONEXÃO COM O BANCO DE DADOS");
-        
-        UsuarioModel usuarioModel = new UsuarioModel(2, "Gabriel Tavares", "111111111", null, "xxxx@gmail.com", 2);
-        UsuarioDAO usuarioDao = new UsuarioDAO();
-//        usuarioDao.cadastrarUsuario(usuarioModel); // Agora está passando um objeto UsuarioModel
-        
-//        usuarioDao.delUsuario(usuarioModel);
-        
+
         try {
-            System.out.println("Atualizando usuário com ID: " + usuarioModel.getCdUsuario());
-            usuarioDao.atualizarUsuario(usuarioModel);
-            System.out.println("Usuário atualizado com sucesso!");
+            UsuarioDAO usuarioDao = new UsuarioDAO();
+
+            int cdUsuarioParaFiltrar = 3; // Substitua pelo ID desejado para filtrar os usuários
+            List<UsuarioModel> listaUsuarios = usuarioDao.listarUsuariosPorCdUser(cdUsuarioParaFiltrar);
+
+            for (UsuarioModel usuario : listaUsuarios) {
+                System.out.println("ID: " + usuario.getCdUsuario());
+                System.out.println("Nome: " + usuario.getNomeUsuario());
+                System.out.println("CPF: " + usuario.getNumeroCPF());
+                System.out.println("Email: " + usuario.getDescricaoEmail());
+            }
         } catch (SQLException e) {
-            System.err.println("Erro ao atualizar usuário:");
+            System.err.println("Erro ao listar usuários por ID:");
             e.printStackTrace();
         }
-        
-        //usuarioDao.atualizarUsuario(usuarioModel);
-
-        
-//        cd_user, nm_user, nr_CPF, dt_nasc, ds_email
-        
-
-	}
+    }
 }
